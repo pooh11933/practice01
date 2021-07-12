b28c8cea5779deec1fc4c0eea16484c9b8cb2149
pooh11933 <pooh11933@ajou.ac.kr>
2021-07-12 16:18:50 +0900

    7/12 1ban class commit 4

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -2,10 +2,12 @@ from Tools.scripts.serve import app
 from django.urls import path
 
 import accountapp
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('hello_world/', hello_world, name = 'hello_world'),
+    path('hello_world/', hello_world, name='hello_world'),
+
+    path('create/', AccountCreateView.as_view(), name='create'),
 ]
