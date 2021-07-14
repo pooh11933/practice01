2eddcd0fc0f032db9d1c59b7558ccc62dcc4c623
pooh11933 <pooh11933@ajou.ac.kr>
2021-07-14 09:47:35 +0900

    7/14 1ban class commit 2

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,4 +1,5 @@
 from Tools.scripts.serve import app
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 import accountapp
@@ -8,6 +9,10 @@ app_name = 'accountapp'
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'),
+         name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/', AccountCreateView.as_view(), name='create'),
 ]
--- practice/settings.py
+++ practice/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.urls import reverse_lazy
+
 BASE_DIR = Path(__file__).resolve().parent.parent #시크릿키를 .env에 따로 저장하고 불러오기
 
 env_list = dict()
@@ -118,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'UTC'
 
@@ -141,3 +143,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
