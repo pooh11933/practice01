23e4a0266d6575cffc8dbc7450309011d5833ee5
pooh11933 <pooh11933@ajou.ac.kr>
2021-07-29 15:17:55 +0900

    7/29 1ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -32,12 +32,13 @@ def hello_world(request):
                           context={'hello_world_list': hello_world_list})
 
 
-
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 
 class AccountDetailView(DetailView):
@@ -55,8 +56,10 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 
 @method_decorator(has_ownership, 'get')
