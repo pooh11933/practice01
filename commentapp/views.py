580e2ea0ca389aeec2a986eb504e194de6a7b141
pooh11933 <pooh11933@ajou.ac.kr>
2021-08-09 15:40:52 +0900

    8/9 1ban class commit 3

--- commentapp/views.py
+++ commentapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 
 from articleapp.models import Article
 from commentapp.forms import CommentCreationForm
@@ -16,8 +16,18 @@ class CommentCreateView(CreateView):
 
     def form_valid(self, form):
         form.instance.writer = self.request.user
-        form.instance.article_id = self.request.POST.get('article.pk')
+        form.instance.article_id = self.request.POST.get('article_pk')
         return super().form_valid(form)
 
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
+
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk}
+        )
