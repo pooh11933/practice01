68ed97f8d1c3a60ba569e2f4b66ea95b147558ba
pooh11933 <pooh11933@ajou.ac.kr>
2021-09-09 16:06:53 +0900

    9/9 1ban class commit 1

--- practice/settings/base.py
+++ practice/settings/base.py
@@ -15,32 +15,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.urls import reverse_lazy
 
-BASE_DIR = Path(__file__).resolve().parent.parent #시크릿키를 .env에 따로 저장하고 불러오기
+BASE_DIR = Path(__file__).resolve().parent.parent.parent  #시크릿키를 .env에 따로 저장하고 불러오기
 
-env_list = dict()
-
-local_env = open(os.path.join(BASE_DIR, '.env')) # BASE_DIR + .env 경로합쳐서 local_env에
-
-while True:
-    line = local_env.readline()     # 라인 한 줄을 읽어온다
-    if not line:
-        break
-    line = line.replace('\n', '')   # 줄바꿈줄은 라인 마지막에 항상 있기 때문에 없애줌
-    start = line.find('=')          # 구분자
-    key = line[:start]              # 'SECRET_KEY'
-    value = line[start+1:]          # key
-    env_list[key] = value           # env_list['SECRET_KEY'] = key
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -100,15 +79,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'practice.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
