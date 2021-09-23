1dc09009670fda26f427fa73dc26a0f31ef30f90
pooh11933 <pooh11933@ajou.ac.kr>
2021-09-23 15:48:50 +0900

    9/23 1ban class commit 1

--- practice/settings/deploy.py
+++ practice/settings/deploy.py
@@ -1,22 +1,15 @@
 from .base import *
 
+def read_secrets(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
 
-env_list = dict()
-
-local_env = open(os.path.join(BASE_DIR, '.env')) # BASE_DIR + .env 경로합쳐서 local_env에
-
-while True:
-    line = local_env.readline()     # 라인 한 줄을 읽어온다
-    if not line:
-        break
-    line = line.replace('\n', '')   # 줄바꿈줄은 라인 마지막에 항상 있기 때문에 없애줌
-    start = line.find('=')          # 구분자
-    key = line[:start]              # 'SECRET_KEY'
-    value = line[start+1:]          # key
-    env_list[key] = value           # env_list['SECRET_KEY'] = key
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
+SECRET_KEY = read_secrets('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -31,8 +24,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': 'password1234',
+        'USER': read_secrets('MARIADB_USER'),
+        'PASSWORD': read_secrets('MARIADB_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
